b0474326d29b5278a1807ba8d8b90c2fb847a9ac
Hassan Sajjad Khan <70847096+hassansajjadkhan@users.noreply.github.com>
2025-07-28 15:36:51 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,3 +7,13 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
+
+const path = require('path');
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Catch-all route to serve React index.html on any unmatched route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
